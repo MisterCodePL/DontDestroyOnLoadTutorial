26a9d5d628d7e0e1e2b3e673eb050d76ac0399c9
MisterCodePL <mistercode11@gmail.com>
2017-08-01 11:31:45 +0200

    uploadedTutorial

---
+++ Assets/DataContainer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DataContainer : MonoBehaviour
+{
+    public static DataContainer dataContainer;
+
+    public string VariableToStoreBetweenScenes { get; set; } //Dane, które chcemy przechować między scenami.
+
+    private void Awake()
+    {
+        if(dataContainer==null)
+        {
+            DontDestroyOnLoad(gameObject); //DontDestroyOnLoad zapobiega niszczeniu się obiektu przy ładowaniu nowej sceny
+            dataContainer = this;
+        }
+        else if(dataContainer!= this) //Zapobiega możliwości istnieniu dwóch DataContainerów w jednej scenie.
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/LoadNewScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewScene : MonoBehaviour
+{
+    public void OnClick()
+    {
+        if (SceneManager.GetActiveScene().name.Equals("Scene1"))
+            SceneManager.LoadScene("Scene2");
+        else
+            SceneManager.LoadScene("Scene1");
+    }
+
+}
