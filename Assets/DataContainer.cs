acca7218d322ecd04bc5b31f79ca825efdde910d
MisterCodePL <mistercode11@gmail.com>
2017-08-02 16:58:28 +0200

    changed GameObject.Find() to calling Singleton instance.

--- Assets/DataContainer.cs
+++ Assets/DataContainer.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
-
+//DataContainer jest tak naprawdę Singletonem
 public class DataContainer : MonoBehaviour
 {
-    public static DataContainer dataContainer;
+    public static DataContainer Instance;
 
-    public string VariableToStoreBetweenScenes { get; set; } //Dane, które chcemy przechować między scenami.
+    public string VariableToStoreBetweenScenes { get; set; } //Dane do przechowania.
 
     private void Awake()
     {
-        if(dataContainer==null)
+        if(Instance==null)
         {
-            DontDestroyOnLoad(gameObject); //DontDestroyOnLoad zapobiega niszczeniu się obiektu przy ładowaniu nowej sceny
-            dataContainer = this;
+            DontDestroyOnLoad(gameObject); //DontDestroyOnLoad zapobiega niszczeniu się obiektu przy 
+                                            //ładowaniu nowej sceny
+            Instance = this;
         }
-        else if(dataContainer!= this) //Zapobiega możliwości istnieniu dwóch DataContainerów w jednej scenie.
+        else if(Instance!= this) //Zapobiega możliwości istnieniu dwóch DataContainerów w jednej scenie.
         {
             Destroy(gameObject);
         }
--- Assets/InputFieldScript.cs
+++ Assets/InputFieldScript.cs
@@ -4,23 +4,19 @@ using UnityEngine.UI;
 public class InputFieldScript : MonoBehaviour
 {
     private InputField inputField; //Referencja do pola tekstowego.
-    private DataContainer dataContainer; //Referencja do kontenera na dane.
 
-    private void Start() /*Na początku sceny pobieramy referencje do DataContainera oraz 
-        do pola tekstowego.*/
+    private void Start() /*Na początku sceny pobieramy referencje do do pola tekstowego.*/
     {
-        dataContainer = GameObject.Find("EmptyObjectName").GetComponent<DataContainer>(); 
-
         inputField = GetComponent<InputField>();
-        if(dataContainer.VariableToStoreBetweenScenes !=null) /*Sprawdzenie czy zmienna ma już jakąś 
-            wartość (czy jest to już kolejna scena.)*/
-            inputField.text = dataContainer.VariableToStoreBetweenScenes; /*przypisanie wartości z 
-        DataContainera do pola tekstowego.*/
+        if(DataContainer.Instance.VariableToStoreBetweenScenes !=null) /*Sprawdzenie czy zmienna ma już 
+            jakąś wartość (czy jest to już kolejna scena.)*/
+            inputField.text = DataContainer.Instance.VariableToStoreBetweenScenes; /*przypisanie 
+        wartości z DataContainera do pola tekstowego.*/
     }
 
     private void OnDisable() //Przy końcu sceny zapisanie zawartości pola tekstowego do DataContainera.
     {
-        dataContainer.VariableToStoreBetweenScenes = inputField.text;
+        DataContainer.Instance.VariableToStoreBetweenScenes = inputField.text;
     }
 
 }
